fea55318748335e96216f1ae5a4bb21c4d04f6a5
SeanMcReynolds <sean.mcreynolds066@gmail.com>
2023-10-02 12:38:12 -0500

    Initial commit. Created screens and linked first question to correct and incorrect as well as linked those to the second question

---
+++ main.py
@@ -0,0 +1,33 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+
+Builder.load_file("QuizPage.kv")
+class QuizPageApp(App):
+    def build(self):
+        return QuizManager()
+
+class QuizManager(ScreenManager):
+    pass
+
+class Question1Screen(Screen):
+    def answer_question(self, bool):
+        if bool:
+            self.manager.current = "correct"
+        else:
+            self.manager.current = "incorrect"
+
+
+
+class Question2Screen(Screen):
+    pass
+
+class CorrectScreen(Screen):
+    def advance(self):
+        self.manager.current = "question two"
+
+class IncorrectScreen(Screen):
+    def advance(self):
+        self.manager.current = "question two"
+
+QuizPageApp().run()
